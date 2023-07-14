7d986b851149d1bfac66ee9b08e477118c862994
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-07-14 11:39:44 +0300

    Added tests for PaginationFilter helper class.

---
+++ PaymentsBudgetSystem.Tests/Helpers/PaginationFilterTests.cs
@@ -0,0 +1,37 @@
+﻿namespace PaymentsBudgetSystem.Tests.Helpers
+{
+    using PaymentsBudgetSystem.Core.Helpers;
+    using static Common.DataConstants.General;
+
+    [TestFixture]
+    internal class PaginationFilterTests
+    {
+        [Test]
+        public void FilterItemsByPage_ReturnsCorrectNumberOfElements()
+        {
+            int page = 2;
+
+            var items = new List<object>()
+            {
+                new object(),
+                new object(),
+                new object(),
+                new object(),
+                new object(),
+                new object(),
+                new object(),
+                new object(),
+                new object(),
+                new object(),
+                new object(),
+                new object(),
+            };
+
+            var paginationFilter = new PaginationFilter<object>();
+
+            var result = paginationFilter.FilterItemsByPage(items, page);
+
+            Assert.That(result.Count, Is.EqualTo(items.Count - ItemsPerPage));
+        }
+    }
+}
